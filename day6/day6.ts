e52b1a9955fec4a78f9cb872b3d345c55a864702
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2022-12-06 09:03:27 +0100

    day6

---
+++ day6/day6.ts
@@ -0,0 +1,30 @@
+const fs = require('fs');
+import { distinct, intersect, range } from 'powerseq';
+
+function loadFile() {
+    const file: string = fs.readFileSync('./day6/input.txt', "utf-8");
+    return file.split('');
+}
+
+function checkPackage(chars: string[]){
+    return [...distinct(chars)].length === chars.length;
+}
+
+function findMarker(packegeLength){
+    const file = loadFile();
+    let i = packegeLength;
+    let marker = undefined;
+    while(i <= file.length && !marker){
+        const p = file.slice((i -packegeLength),i);
+        if(checkPackage(p)){
+            marker = p;
+            break;
+        }
+        i+=1;
+    }
+    return {marker, index: i};
+}
+/**Round 1 */
+console.log(findMarker(4));
+/**Round 2 */
+console.log(findMarker(14));
