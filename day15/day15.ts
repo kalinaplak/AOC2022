ae61bd0269ae386f5c03299f6dcd7249cba0942c
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2022-12-16 20:34:08 +0100

    day15 part 2

--- day15/day15.ts
+++ day15/day15.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import { filter, map, orderby, pipe, toarray } from 'powerseq';
 
 type Point = [number, number];
 
@@ -38,7 +39,7 @@ function calculateRound1() {
                 points.add(i);
             }
         }
-    }) 
+    })
 
     sensors.forEach(s => {
         if (s.beacon[1] === resultY)
@@ -48,4 +49,49 @@ function calculateRound1() {
     return points.size;
 }
 
+/* 2 */
+function getRowIntervals(sensors: Sensor[], row: number): { from: number, to: number }[] {
+    return pipe(
+        sensors,
+        map(({ sensor, distance }) => {
+            const [sX, sY] = sensor;
+            const rowDistance = Math.abs(sY - row);
+            if (rowDistance > distance) {
+                return;
+            }
+            return { from: sX - (distance - rowDistance), to: sX + (distance - rowDistance) }
+        }),
+        filter(s => !!s),
+        orderby(s => s.from),
+        toarray()
+    )
+}
+
+function joinLinesIfPossible([newFrom, newTo], [previousFrom, previousTo]) {
+    return (newFrom > previousTo)
+        ? [[previousFrom, previousTo], [newFrom, newTo]]
+        : [[Math.min(newFrom, previousFrom), Math.max(newTo, previousTo)]]
+}
+
+function processLine(sensors, row) {
+    const rowIntervals = getRowIntervals(sensors, row);
+    return rowIntervals.reduce((acc, curr) => {
+        if (acc.length === 0) return [[curr.from, curr.to]];
+        return [...acc.slice(0, acc.length - 1), ...joinLinesIfPossible([curr.from, curr.to], acc[acc.length - 1])]
+    }, []);
+}
+
+function calculateRound2() {
+    const sensors = loadFile();
+    const max = 4000000;
+    for (let i = 0; i < max; i++) {
+        const rowIntervals = processLine(sensors, i);
+        if (rowIntervals.length > 1) {
+            const gapX = rowIntervals[0][1] + 1;
+            return (gapX * max) + i;
+        }
+    }
+}
+
 console.log(calculateRound1());
+console.log(calculateRound2());
